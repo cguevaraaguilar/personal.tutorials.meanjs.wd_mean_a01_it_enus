59b408f9fbd086dd8ede4d1b3c495bf6835a5462
cguevara <cguevara@localhost.localdomain>
2017-04-13 23:01:51 -0400

    Automated commit

--- Express/articles/public/javascripts/articleApp.js
+++ Express/articles/public/javascripts/articleApp.js
@@ -1,5 +1,6 @@
-// Se indica la referencia a route
-var app = angular.module ('articleApp', ['ngRoute']);
+// Se agrega la dependencia a Route
+// Se agrega la dependencia a Resouce
+var app = angular.module ('articleApp', ['ngRoute', 'ngResource']);
 
 // función de proveedor de ruteo
 app.config (function ($routeProvider) {
@@ -14,8 +15,28 @@ app.config (function ($routeProvider) {
 		})
 });
 
+/*
+// Crea el servicio
+app.factory ('articleService', function ($http) {
+
+	var factory = {};
+	factory.getArticles = function () {
+
+		return $http.get ('/api/articles');
+	}
+
+	return factory;
+});
+*/
+
+// Se sustituye $http por $resource
+app.factory ('articleService', function ($resource) {
+
+	return $resource('/api/articles');
+});
+
 // Se agregó $http para indicar la dependencia
-app.controller('mainController', function ($scope, $http) {
+app.controller('mainController', function ($scope, $http, articleService) {
 
 	$scope.articles = [];
 	$scope.newArticle = {
@@ -26,7 +47,7 @@ app.controller('mainController', function ($scope, $http) {
 	};
 
 	/*
-	Quita la operación en memoria
+	Operación en memoria
 	$scope.post = function () {
 		$scope.newArticle.timestamp = Date.now();
 		$scope.articles.push ($scope.newArticle);
@@ -39,21 +60,43 @@ app.controller('mainController', function ($scope, $http) {
 	};
 	*/
 
+	/*
 	// Obtiene los artículos insertados
 	$http.get ('/api/articles').then (function (response) {
 
 		$scope.articles = response.data;
 	});
+	*/
+
+	/*
+	Se elimina la llamada al servicio que empleaba $http
+	articleService.getArticles().then (function(response) {
+
+		$scope.articles = response.data;
+	});
+	*/
+
+	$scope.articles = articleService.query ();
 
 	// inserta el artículo
 	$scope.post = function () {
 
 		$scope.newArticle.timestamp = Date.now ();
+
+		/*
+		Se elimina la llamda http al post
 		$http.post ('/api/articles', $scope.newArticle).then (function (response) {
 
 			$scope.articles.push (response.data);
 			$scope.newArticle = {username: '', title: '', text: '', timestamp: ''};
 		});
+		*/
+
+		articleService.save ($scope.newArticle, function () {
+
+			$scope.articles.push ($scope.newArticle);
+			$scope.newArticle = {username: '', title: '', text: '', timestamp: ''};
+		})
 	}
 
 });
